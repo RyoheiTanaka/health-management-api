6853ba9c2c0958d237b03e6de9505ad282f097a1
coolat1010 <ryohei632@gmail.com>
2025-01-28 01:52:54 +0900

    ミドルウェア修正

--- app/Http/Middleware/CheckIP.php
+++ app/Http/Middleware/CheckIP.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class CheckIP
@@ -19,6 +20,9 @@ class CheckIP
         $allowedIps = [
             env('AWS_LAMBDA_EIP'),
         ];
+
+        Log::info('allowedIps = ' . implode(',', $allowedIps));
+        Log::info('requestIp = ' . $request->ip());
 
         // リクエスト元IPが許可されていない場合
         if (!in_array($request->ip(), $allowedIps)) {
