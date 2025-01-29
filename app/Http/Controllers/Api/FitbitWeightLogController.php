1b082608920bc15ab520ca147bed26376a0b50ee
coolat1010 <ryohei632@gmail.com>
2025-01-29 09:58:39 +0900

    体重一覧APIパラメータ追加

--- app/Http/Controllers/Api/FitbitWeightLogController.php
+++ app/Http/Controllers/Api/FitbitWeightLogController.php
@@ -3,20 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\FitbitWeightLog\IndexFitbitWeightLogFormRequest;
 use App\Http\Requests\Api\FitbitWeightLog\ShowFitbitWeightLogFormRequest;
 use App\Models\FitbitWeightLog;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 
 class FitbitWeightLogController extends Controller
 {
     /**
      * Handle the incoming index request.
      */
-    public function index(Request $request)
+    public function index(IndexFitbitWeightLogFormRequest $request)
     {
-        $date = (new Carbon)->subWeek()->format('Y-m-d');
-        $weightLogs = FitbitWeightLog::where('date', '>=', $date)->orderBy('date')->get();
+        $isDashboard = $request->is_dashboard ?? false;
+
+        $weightLogs = FitbitWeightLog::when($isDashboard, function ($query) {
+            $date = (new Carbon())->subWeek()->format('Y-m-d');
+            return $query->where('date', '>=', $date);
+        })->orderBy('date')->get();
 
         return response([
             'data' => $weightLogs
