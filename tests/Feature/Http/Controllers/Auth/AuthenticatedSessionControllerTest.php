80c8b2bd78e3e8b6567f0696355e6d0ce8a3a682
coolat1010 <ryohei632@gmail.com>
2025-04-17 10:12:10 +0900

    test: テストコード追加
    - Laravelインストール時に追加されたテストファイルを削除
    - ログインのAPIテストを作成

---
+++ tests/Feature/Http/Controllers/Auth/AuthenticatedSessionControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
+
+uses(RefreshDatabase::class);
+
+it('can login', function () {
+    User::create([
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => Hash::make('password'),
+    ]);
+
+    $this->get('/sanctum/csrf-cookie');
+
+    $this->post('/login', [
+        'email' => 'test@example.com',
+        'password' => 'password',
+    ])->assertStatus(200);
+});
